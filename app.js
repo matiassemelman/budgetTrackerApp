ec4c4d46f3c26615b7c3d6fe52cd00a4d44f1a8a
matiassemelman <matiassemelman@gmail.com>
2021-12-02 20:04:20 -0300

    form working

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let transactionForm = document.getElementById("transactionForm");
 
-let transactionFormData = new FormData(transactionForm);
-
-transactionForm.addEventListener('change', function preventingSubmit (event) {
-    event.preventDefault();
- console.log(event);
- console.log(transactionFormData.description)
-
-})
-
+transactionForm.addEventListener("submit", function addingTransaction(event) {
+  event.preventDefault();
+  let transactionFormData = new FormData(transactionForm);
+  let transactionTableRef = document.getElementById("transactionTable");
+  let newTransactionRowRef = transactionTableRef.insertRow(-1);
+  let newTypeCellRef = newTransactionRowRef.insertCell(0);
+  newTypeCellRef.textContent = transactionFormData.get('transactionType');
+  let newDescriptionCellRef = newTransactionRowRef.insertCell(1);
+  newDescriptionCellRef.textContent = transactionFormData.get('description');
+  let newAmountCellRef = newTransactionRowRef.insertCell(2);
+  newAmountCellRef.textContent = transactionFormData.get('amount')
+  let newCategoryCellRef = newTransactionRowRef.insertCell(3);
+  newCategoryCellRef.textContent = transactionFormData.get('category')
+});
