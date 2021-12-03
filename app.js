b7ef50cd79e9bd7246eb6efe385e52c2a246dd7c
matiassemelman <matiassemelman@gmail.com>
2021-12-02 23:52:48 -0300

    modularization of function

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ let transactionForm = document.getElementById("transactionForm");
 transactionForm.addEventListener("submit", function addingTransaction(event) {
   event.preventDefault();
   let transactionFormData = new FormData(transactionForm);
-  let transactionTableRef = document.getElementById("transactionTable");
-  let newTransactionRowRef = transactionTableRef.insertRow(-1);
-  let newTypeCellRef = newTransactionRowRef.insertCell(0);
-  newTypeCellRef.textContent = transactionFormData.get('transactionType');
-  let newDescriptionCellRef = newTransactionRowRef.insertCell(1);
-  newDescriptionCellRef.textContent = transactionFormData.get('description');
-  let newAmountCellRef = newTransactionRowRef.insertCell(2);
-  newAmountCellRef.textContent = transactionFormData.get('amount')
-  let newCategoryCellRef = newTransactionRowRef.insertCell(3);
-  newCategoryCellRef.textContent = transactionFormData.get('category')
+ insertRowTransactionTable(transactionFormData);
 });
+
+function insertRowTransactionTable(transactionFormData) {
+    let transactionTableRef = document.getElementById("transactionTable");
+
+    let newTransactionRowRef = transactionTableRef.insertRow(-1);
+
+    let newTypeCellRef = newTransactionRowRef.insertCell(0);
+    newTypeCellRef.textContent = transactionFormData.get('transactionType');
+
+    newDescriptionCellRef = newTransactionRowRef.insertCell(1);
+    newDescriptionCellRef.textContent = transactionFormData.get('description');
+
+    newAmountCellRef = newTransactionRowRef.insertCell(2);
+    newAmountCellRef.textContent = transactionFormData.get('amount')
+
+    newCategoryCellRef = newTransactionRowRef.insertCell(3);
+    newCategoryCellRef.textContent = transactionFormData.get('category')
+}
