3bab33ab3beb12d1bc4bfdf5d7eb462a0ea884d2
matiassemelman <matiassemelman@gmail.com>
2021-12-06 21:32:52 -0300

    adding comments for every step

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 let transactionForm = document.getElementById("transactionForm");
+// Selecting form
+
 
 transactionForm.addEventListener("submit", function addingTransaction(event) {
+  // adding event listener
   event.preventDefault();
+  // preventing page reload on submit
+  
   let transactionFormData = new FormData(transactionForm);
+  // convert the form data into a FormData constructor.
+  
   let transactionObject = formDataToTransactionObject(transactionFormData)
+  // initializing var with object created by the function in line 21, which receives the form data converted.
+
   saveTransactionObject(transactionObject);
+  // storing the object into the localStorage
+
  insertRowTransactionTable(transactionObject);
+  // Inserting row and cells with the keys/values of the transactionObject
+
 });
 
 
@@ -43,7 +56,11 @@ function insertRowTransactionTable(transactionObject) {
 
 function saveTransactionObject(transactionObject) {
   let myTransactionArray = JSON.parse(localStorage.getItem("transactionData")) || [];
+  // If getItem doesn't get something (empty localStorage), initialize var with empty array
   myTransactionArray.push(transactionObject);
+  // Add the transaction object to the array of transactions
 let transactionArrayJSON = JSON.stringify(myTransactionArray);
+  // convert the array to JSON to store it in localStorage
 localStorage.setItem('transactionData', transactionArrayJSON);
+  // storage Item
 }
